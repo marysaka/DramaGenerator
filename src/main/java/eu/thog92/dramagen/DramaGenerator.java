ad5ceef5b2c3f4054712faa88d86b4da20cbb801
Thog <thog92@hotmail.fr>
2015-03-26 21:57:15 +0100

    WIP Changes for Thog/BotGenerator@5373aba565fd528a674b4bb3510d0eba08d59007

--- src/main/java/eu/thog92/dramagen/DramaGenerator.java
+++ src/main/java/eu/thog92/dramagen/DramaGenerator.java
@@ -5,11 +5,14 @@ import com.sun.net.httpserver.HttpServer;
 import eu.thog92.generator.api.BotGenerator;
 import eu.thog92.generator.api.annotations.Module;
 import eu.thog92.generator.api.annotations.SubscribeEvent;
-import eu.thog92.generator.api.events.HttpInitEvent;
+import eu.thog92.generator.api.events.HttpStartEvent;
 import eu.thog92.generator.api.events.InitEvent;
 import eu.thog92.generator.api.tasks.ScheduledTask;
 import eu.thog92.generator.core.tasks.GeneratorTask;
 import eu.thog92.generator.core.tasks.TwitterTask;
+import eu.thog92.generator.twitter.TwitterModule;
+
+import java.io.IOException;
 
 @Module(name = "Drama", version = "1.1")
 public class DramaGenerator
@@ -17,19 +20,28 @@ public class DramaGenerator
     private BotGenerator generator;
     private GeneratorTask generatorTask;
     private ScheduledTask twitterTask;
+    private HttpServer server;
 
     @SubscribeEvent
     public void init(InitEvent event)
     {
         generator = event.getBotGenerator();
+        //Config config = generator.getConfig();
         this.generatorTask = new GeneratorTask();
-        this.twitterTask = new TwitterTask(this.generatorTask);
-        this.twitterTask.setDelay(generator.getConfig().delay);
+        this.twitterTask = new TwitterTask(TwitterModule.getInstance().getTwitter(), this.generatorTask, true, null);
+        this.twitterTask.setDelay(3600);
         generator.getTasksManager().scheduleTask(this.twitterTask);
+        /*try
+        {
+            this.server = generator.getHttpManager().createHTTPServer(config.port);
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }*/
     }
 
     @SubscribeEvent
-    public void onHTTPServerInit(HttpInitEvent event)
+    public void onHTTPServerInit(HttpStartEvent event)
     {
         HttpServer server = event.getServer();
         server.createContext("/drama", new DramaHandler(generatorTask, false));
