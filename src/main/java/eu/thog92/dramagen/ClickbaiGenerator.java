032283f98c2c23e491db099854caa250ad1238f5
Thog <thog92@hotmail.fr>
2015-03-27 00:07:44 +0100

    Now have his own config

--- src/main/java/eu/thog92/dramagen/ClickbaiGenerator.java
+++ src/main/java/eu/thog92/dramagen/ClickbaiGenerator.java
@@ -4,12 +4,14 @@ package eu.thog92.dramagen;
 import eu.thog92.generator.api.BotGenerator;
 import eu.thog92.generator.api.annotations.Module;
 import eu.thog92.generator.api.annotations.SubscribeEvent;
+import eu.thog92.generator.api.config.Configuration;
 import eu.thog92.generator.api.events.InitEvent;
 import eu.thog92.generator.api.tasks.ScheduledTask;
 import eu.thog92.generator.core.tasks.GeneratorTask;
 import eu.thog92.generator.core.tasks.TwitterTask;
+import eu.thog92.generator.twitter.TwitterModule;
 
-@Module(name = "Clickbait", version = "1.1")
+@Module(name = "Clickbait", version = "1.1",  dependencies = "after:twitter;")
 public class ClickbaiGenerator
 {
     private BotGenerator generator;
@@ -21,8 +23,18 @@ public class ClickbaiGenerator
     {
         generator = event.getBotGenerator();
         this.generatorTask = new GeneratorTask();
-        this.twitterTask = new TwitterTask(this.generatorTask);
-        this.twitterTask.setDelay(generator.getConfig().delay);
+
+        Configuration configuration = new Configuration(event.getConfigDir(), "ClickBait");
+        ClickbaitConfiguration clickbaitConfiguration = configuration.readFromFile(ClickbaitConfiguration.class);
+        if (clickbaitConfiguration == null)
+        {
+            clickbaitConfiguration = new ClickbaitConfiguration(1800);
+            configuration.saveToDisk(clickbaitConfiguration);
+        }
+
+        this.twitterTask = new TwitterTask(TwitterModule.getInstance().getTwitter(), this.generatorTask, true, null);
+        this.twitterTask.setDelay(clickbaitConfiguration.tweetDelay);
+
         generator.getTasksManager().scheduleTask(this.twitterTask);
     }
 }
---
+++ src/main/java/eu/thog92/dramagen/ClickbaitConfiguration.java
@@ -0,0 +1,16 @@
+package eu.thog92.dramagen;
+
+public class ClickbaitConfiguration
+{
+    public int tweetDelay;
+
+    public ClickbaitConfiguration()
+    {
+
+    }
+
+    public ClickbaitConfiguration(int delay)
+    {
+        this.tweetDelay = delay;
+    }
+}
